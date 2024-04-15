29cdf2b03957b2453939748134b78c9905cade12
Jose Moises <jmvilla2301@gmail.com>
2024-04-15 14:44:32 -0500

    Archivos Creados por Villanueva

---
+++ src/uc1_villanueva/Preg01.java
@@ -0,0 +1,10 @@
+
+package uc1_villanueva;
+
+
+public class Preg01 {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/uc1_villanueva/Preg02.java
@@ -0,0 +1,7 @@
+package uc1_villanueva;
+
+public class Preg02 {
+    public static void main(String[] args) {
+
+    }
+}
