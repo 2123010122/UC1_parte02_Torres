78685339a6b710e6374734a61caf2edeb5acbf32
Henry Torres <2123010122@untels.edu.pe>
2024-04-15 14:55:19 -0500

    Archivos creados por Torres

---
+++ src/uc1_villanueva/Preg03.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uc1_villanueva;
+
+/**
+ *
+ * @author Dell
+ */
+public class Preg03 {
+    
+}
